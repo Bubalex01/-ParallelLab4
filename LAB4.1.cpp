f594960136d4c4688810c087f79871fe0f3d1299
Bubalex01 <46608714+Bubalex01@users.noreply.github.com>
2024-03-31 21:43:37 +0500

    Add files via upload

---
+++ LAB4.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+#include "mpi.h"
+
+int main(int argc, char *argv[])
+{	
+	int numt, rank;
+	int rowsForA, extraRows, rows; 
+	int buff = 0;
+	MPI_Status status;
+	double beginTime, endTime;
+	
+	
+	MPI_Finalize();
+
+}	
